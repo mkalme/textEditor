e2e0120f070c54d7162881fbe0ab9428c6568c3f
mkalme <mikelis.kalme@gmail.com>
2025-05-22 15:23:13 +0300

    Add project files.

---
+++ textEditor/Editor.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace textEditor
+{
+    partial class Editor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Editor));
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.richTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
+            this.richTextBox.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(1062, 620);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            this.richTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1062, 620);
+            this.panel1.TabIndex = 1;
+            // 
+            // Editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(1086, 644);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Editor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Editor";
+            this.Load += new System.EventHandler(this.Editor_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ textEditor/Editor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XmlFileEngine;
+using System.IO;
+using System.Threading;
+
+namespace textEditor
+{
+    public partial class Editor : Form
+    {
+        private static Commands xmlCommands;
+        private static string path = "";
+
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void Editor_Load(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length > 1)
+            {
+                xmlCommands = new Commands(args[1].Split('\n')[0]);
+                path = args[1].Split('\n')[1];
+
+                WaitTillFileIsReady(xmlCommands.BasePath);
+
+                richTextBox.Text = xmlCommands.GetFileAttribute(path, "text");
+
+                Text = "Editor | " + xmlCommands.BasePath + " | " + path;
+            }
+        }
+
+        private void save() {
+            WaitTillFileIsReady(xmlCommands.BasePath);
+
+            xmlCommands.ChangeFileAttribute(path, "text", richTextBox.Text);
+        }
+
+        public static void WaitTillFileIsReady(string filename)
+        {
+            while (!IsFileReady(filename))
+            {
+                Thread.Sleep(30);
+            }
+        }
+        public static bool IsFileReady(string filename)
+        {
+            try
+            {
+                using (Stream stream = new FileStream(filename, FileMode.Open))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void richTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.S && e.Modifiers == Keys.Control)
+            {
+                save();
+            }
+        }
+    }
+}
